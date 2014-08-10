6bd34ceb47a1854f7c03f92ff8d75281e68442c0
Jesse Mullan <jmullan@visi.com>
2014-08-10 13:27:52 -0700

    Update example

--- example.php
+++ example.php
@@ -1,23 +1,8 @@
 <?php
-require_once('SessionHandler.php');
-
-$session = new SessionHandler();
-
-// add db data
-$session->setDbDetails('localhost', 'username', 'password', 'database');
-
-// OR alternatively send a MySQLi ressource
-// $session->setDbConnection($mysqli);
-
-$session->setDbTable('session_handler_table');
-session_set_save_handler(array($session, 'open'),
-                         array($session, 'close'),
-                         array($session, 'read'),
-                         array($session, 'write'),
-                         array($session, 'destroy'),
-                         array($session, 'gc'));
-
-// The following prevents unexpected effects when using objects as save handlers.
-register_shutdown_function('session_write_close');
-
+require_once('Jmullan/Sessions/vendor/autoload.php');
+$handler = new \Jmullan\Sessions\Handler();
+$handler->setDbDetails($session_dsn, 'username', 'password');
+$handler->setDBTable('sessions');
+session_set_save_handler($handler, true);
 session_start();
+
